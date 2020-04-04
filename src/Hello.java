1e44feb21f66eebfd1e63bd12517b698f0d38bea
Islom11 <islom.k1995@gmail.com>
2020-04-04 09:42:40 +0500

    salom f

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Salom");
         System.out.println("Kimdir");
+        System.out.println("Qachondir");
 
     }
 }
